139e2b6fb08bee000eb43bfc5b596a7782e5e84b
Akshat Srivastava <113674819+akshatgithubgre@users.noreply.github.com>
2024-06-08 02:00:56 +0530

    make_response() method and http response code

--- 7-June/Flask-Task/venv/model/user_model.py
+++ 7-June/Flask-Task/venv/model/user_model.py
@@ -1,5 +1,6 @@
 import json
 import mysql.connector
+from flask import make_response
 class user_model():
     def __init__(self):
         # Connections estabilishment code
@@ -17,9 +18,9 @@ class user_model():
         result=self.cur.fetchall()
         if(len(result)>0):
             # return json.dumps(result)#dumps function turn to convert that serialized object(result) to string format but we have to generate a better readable format
-            return {"payload":result}#this will return json data as postman api tool will beautify it whereas for dumps postamn will not beautify it
+            return make_response({"payload":result},200)#this will return json data as postman api tool will beautify it whereas for dumps postamn will not beautify it
         else:
-            return {"message":"No Data found"}
+            return make_response({"message":"No Data found"},204)#204 doesnt need a body
         
 
     def user_addone_model(self,data):
@@ -27,19 +28,19 @@ class user_model():
         # print(data)
         self.cur.execute(f"INSERT INTO users(name,email,phone,role,password) VALUES ('{data['name']}','{data['email']}','{data['phone']}','{data['role']}','{data['password']}')")
         # print(data['email'])
-        return {"message":"User Created Successfully"}
+        return make_response({"message":"User Created Successfully"},201)
     
 
     def user_update_model(self,data):
         self.cur.execute(f"UPDATE users SET name='{data['name']}',email='{data['email']}',phone='{data['phone']}',role='{data['role']}',password='{data['password']}' WHERE id= {data['id']}")
         if self.cur.rowcount>0:
-            return {"message":"User Updated Successfully"}
+            return make_respone({"message":"User Updated Successfully"},201)
         else:
-            return {"message":"Nothing to Update"}
+            return make_response({"message":"Nothing to Update"},202)
         
     def user_delete_model(self,id):
         self.cur.execute(f"DELETE FROM users WHERE id= {id}")
         if self.cur.rowcount>0:
-            return {"message":"User Deleted Successfully"}
+            return make_response({"message":"User Deleted Successfully"},200)
         else:
-            return {"message":"Nothing to Delete"}
+            return make_response({"message":"Nothing to Delete"},202)
