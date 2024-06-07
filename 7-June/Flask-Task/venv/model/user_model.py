a87e8b19a1f78218e6229019ca2cb2664573c7ea
Akshat Srivastava <113674819+akshatgithubgre@users.noreply.github.com>
2024-06-08 00:35:12 +0530

    sql work starts here

--- 7-June/Flask-Task/venv/model/user_model.py
+++ 7-June/Flask-Task/venv/model/user_model.py
@@ -1,5 +1,20 @@
+import json
 import mysql.connector
 class user_model():
-    def user_signup_model(self):
-        #Bussiness Logic
-        return "This is user_signup_model"
+    def __init__(self):
+        # Connections estabilishment code
+        try:
+            self.con=mysql.connector.connect(host="localhost",username="root",password="fill_the_password",database="flask_tutorial")
+            self.cur=self.con.cursor(dictionary=True)
+            print("Connection successfull")
+        except:
+            print("Some error")
+    def user_getall_model(self):
+        # Connection estabilishment code
+        # Query execution code
+        self.cur.execute("SELECT * FROM users")
+        result=self.cur.fetchall()
+        if(len(result)>0):
+            return json.dumps(result)
+        else:
+            return "No Data found"
